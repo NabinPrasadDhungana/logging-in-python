88928ba57542d89d803d46b95ab9adb6c850249f
Nabin Prasad Dhungana <prnabin123@gmail.com>
2026-01-14 12:07:44 +0545

    Logging added to the calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+import logging
+from calculator_logger import setup_logger
+
 def add(a, b):
     return a + b
 
@@ -16,6 +19,11 @@ def power(a, b):
 def root(a, b):
     return a ** (1/b)
 
+logger = setup_logger()
+logger.debug("=" * 40)
+logger.info("Calculator intantiated!")
+logger.debug("=" * 40)
+
 while True:
     print("1. Add       2. Subtract")
     print("3. Multiply  4. Divide")
@@ -28,25 +36,38 @@ while True:
 
         if user_input == '1':
             print(f"Sum of {a} and {b} is ", add(a, b))
+            logger.info('Addition performed')
 
         elif user_input == '2':
             print(f"Subtraction of {a} and {b} is ", subtract(a, b))
+            logger.info('Subtraction performed')
 
         elif user_input == '3':
             print(f"Multiplication of {a} and {b} is ", multiply(a, b))
+            logger.info('Multiplication performed')
 
         elif user_input == '4':
-            print(f"Division of {a} and {b} is ", divide(a, b))
+            try:
+                print(f"Division of {a} and {b} is ", divide(a, b))
+                logger.info('Division performed')
+            except ZeroDivisionError:
+                print("Tried to divide a number by Zero (0)")
+                logger.error("Tried to divide a number by Zero (0)")
 
         elif user_input == '5':
             print(f"Power of {a} and {b} is ", power(a, b))
+            logger.info('Indices performed')
 
         else:
             print(f"Root of {a} and {b} is ", root(a, b))
+            logger.info('Root performed')
         
         choice = input("Do you want to continue? (y/n): ")
         if choice == 'n':
+            logger.info("User opted to stop calculation.\n")
             break
+        logger.info("User opted to continue calculation.")
     else:
         print("Invalid input!")
+        logger.error("User input is invalid.")
         
